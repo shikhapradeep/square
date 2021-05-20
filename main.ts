58d9c5d52a78a297252f73f3fc681df6e8430a10
shikhapradeep <84522716+shikhapradeep@users.noreply.github.com>
2021-05-20 21:55:03 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+    turtle.forward(1)
+    turtle.turnRight()
+    turtle.forward(1)
+    turtle.turnRight()
+    turtle.forward(1)
+    turtle.turnRight()
+    turtle.forward(1)
+    turtle.turnRight()
+})
